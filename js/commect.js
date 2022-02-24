5f80f3838fc4f8ad62b3ae5111c223cdbe6e74af
monirhosenakanda <monirhosenakanda@gmail.com>
2022-02-24 09:55:13 +0600

    kaneya api comment consloe and show in website

--- js/commect.js
+++ js/commect.js
@@ -1,7 +1,22 @@
 const loadComment = () =>{
     fetch('https://jsonplaceholder.typicode.com/comments')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayComment(data))
 };
 
-loadComment();
+const displayComment = comments =>{
+    console.log(comments)
+    const commentElement = document.getElementById('displaycomment');
+    for (const coment of comments){
+        const div = document.createElement('div')
+        div.classList.add('commentshow');
+        div.innerHTML = `
+        <h2>Name : ${coment.name}</h2>
+        <h3>Email: ${coment.email}</h3>
+        <h4>Commet: ${coment.body}</h4>
+        
+        `;
+        commentElement.appendChild(div);
+    }
+
+}
